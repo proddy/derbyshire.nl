a4759c904fd4c7ecb354aef95131dda62aa07a65
proddy <paul@derbyshire.nl>
2021-04-19 17:55:02 +0200

    updated email form

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,22 @@
 */
 
 if ('addEventListener' in window) {
-  window.addEventListener('load', function() { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
+  window.addEventListener('load', function () { document.body.className = document.body.className.replace(/\bis-loading\b/, ''); });
   document.body.className += (navigator.userAgent.match(/(MSIE|rv:11\.0)/) ? ' is-ie' : '');
 }
 
-$(function(){
-  
+$(function () {
+
   // Hover text for social buttons
   $('.icons a').hover(
-    function() { $('#linkHoverText').text( $(this).clone().children().remove().end().text() ); },
-    function() { $('#linkHoverText').text(''); }
+    function () { $('#linkHoverText').text($(this).clone().children().remove().end().text()); },
+    function () { $('#linkHoverText').text(''); }
   );
-  
+
   // Flip the card!
-  $('.flipper').click(function(e){
+  $('.flipper').click(function (e) {
     e.preventDefault();
-    if($(this).attr('href') != '#'){
+    if ($(this).attr('href') != '#') {
       $('.back section').hide();
       $($(this).attr('href')).show();
     }
